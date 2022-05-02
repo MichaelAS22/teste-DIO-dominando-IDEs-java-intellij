98b0658eedbdffbdb54f739151668586dbc9fad7
mikepredador <michael-ale@hotmail.com>
2022-05-01 21:25:13 -0300

    first commit do pai

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+
+import br.com.dio.model.Cat;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        System.out.println(cat);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello world!" + (a+b));*/
+    }
+}
